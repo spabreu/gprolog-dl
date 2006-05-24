452bf062c60f7b966ae5a8b83e5c05527094b7ac
spa <spa>
2006-05-24 17:11:30 +0000

    *** empty log message ***

--- Makefile
+++ Makefile
@@ -1,1 +1,20 @@
-dl.o
+GPLC = gplc
+FPFX = _
+STDOUT = /dev/stdout		# system dependent!
+
+%.so: %.pl
+	( echo .globl $(FPFX)Object_Initializer;	\
+	  echo .globl $(FPFX)System_Directives;		\
+	  echo .globl $(FPFX)User_Directives;		\
+	  $(GPLC) -S -o $(STDOUT) $< ) |		\
+	as -o $@.o;					\
+	ld -dylib -o $@ $@.o;				\
+	rm $@.o
+
+%.o: %.pl
+	$(GPLC) -c $<
+
+all:
+
+clean:
+	rm -f *.so *.o *.s
--- gplc-dl.sh
+++ gplc-dl.sh
@@ -1,3 +1,20 @@
 #! /bin/sh
 
-for F in #*
+for F in $*; do
+    case $F in
+    *.pl) PL_FILES="$PL_FILES '$F'";;
+    -*) FLAGS="$FLAGS $F";;
+    *) echo unknown: $F;;
+    esac
+done
+
+for F in $PL_FILES; do
+    ( echo .globl Object_Initializer
+      echo .globl System_Directives
+      echo .globl User_Directives
+      gplc -S -o /dev/stdout $F )
+    gplc -S $F
+    ( for S in Object_Initializer System_Directives User_Directives; do
+            echo .globl $S; done;
+          cat foo.s ) | as -o foo.o && ld -shared -o foo.so foo.o
+done
