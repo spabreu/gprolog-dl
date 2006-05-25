a69ef1ddc228e9acec9a3e0308cfb65c0108fed1
spa <spa>
2006-05-25 09:38:04 +0000

    import foreign declarations. remove old commented code.

--- dl-interface.pl
+++ dl-interface.pl
@@ -8,9 +8,17 @@
 %  ensure_loaded(UNIT).
 %  load_unit(UNIT).
 %  unload_unit(UNIT).   -- not just yet
-%  clauses for load_path/1 can be used
+%  clauses for load_path/1 can be used 
 
 :- dynamic(load_path/1).
+
+
+:- foreign(dl_open(+codes, +integer, -integer)).
+:- foreign(dl_close(+integer)).
+:- foreign(dl_error(-codes)).
+:- foreign(dl_sym(+integer, +codes, -integer)).
+:- foreign(dl_init(+integer, +integer)).
+
 
 ensure_loaded(UNIT) :- current_unit(UNIT, _), !.
 ensure_loaded(UNIT) :- load_unit(UNIT).
@@ -35,27 +43,12 @@ load_unit_path(UNIT, PATH) :-
 	unit_path_prefix(PFX),
 	format_to_codes(PATH, "~s~w~s", [PFX, UNIT, SFX]).
 
-% dlopen(F) :- dlopen(F, X), g_assign(x, X).
-% dlopen(F, X) :-
-% 	dl_error(_),
-% 	dl_open(F, 257, X),
-% 	( X = 0 ->
-% 	    dl_error(S), format("~s\n", [S]), !, fail
-% 	; true ).
-
-% dlsym(S, V) :- g_read(x, X), dlsym(X, S, V).
-% dlsym(X, S, V) :-
-% 	dl_error(_),
-% 	dl_sym(X, S, V),
-% 	dl_error(E),
-% 	( E = [] -> true
-% 	; format("~s\n", [E]), !, fail ).
-
-% dlinit :- g_read(x, X), dl_init(X, 1).
-
-% dlload(F) :- dlopen(F), dlinit.
 
 % $Log$
+% Revision 1.2  2006/05/25 09:38:04  spa
+% import foreign declarations.
+% remove old commented code.
+%
 % Revision 1.1  2006/05/24 20:44:03  spa
 % *** empty log message ***
 %
